02ce71c6cb61ab5555cd1ae019cb3149fc65a954
Ruslan <rus.baranovskiy@gmail.com>
2022-10-12 11:52:26 +0300

    Fixed UI

--- Calculator/Controllers/MainViewController.swift
+++ Calculator/Controllers/MainViewController.swift
@@ -6,6 +6,18 @@ class MainViewController: UIViewController {
     
     private let mainStackView = MainStackView()
     
+    private let resultLabel: UILabel = {
+        let label = UILabel()
+        label.text = "0"
+        label.font = .boldSystemFont(ofSize: 60)
+        label.adjustsFontSizeToFitWidth = true
+        label.textColor = .white
+        label.textAlignment = .right
+        label.minimumScaleFactor = 0.3
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -17,6 +29,7 @@ class MainViewController: UIViewController {
     private func setupView() {
         view.backgroundColor = .black
         view.addSubview(mainStackView)
+        view.addSubview(resultLabel)
     }
 }
 
@@ -27,8 +40,11 @@ extension MainViewController {
             mainStackView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10),
             mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
             mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
-            mainStackView.heightAnchor.constraint(equalTo: mainStackView.widthAnchor, multiplier: 1)
+            mainStackView.heightAnchor.constraint(equalTo: mainStackView.widthAnchor, multiplier: 1),
             
+            resultLabel.bottomAnchor.constraint(equalTo: mainStackView.topAnchor, constant: -10),
+            resultLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            resultLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10)
         ])
     }
 }
